cc1947d6cc95db814de797a99ff9027d46cff99d
Pradeep Shah <ipradeepshah@gmail.com>
2025-08-04 10:55:08 +0400

    fixing google ownership verfification

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,11 +1,14 @@
 export default function Head(){
     return (
         <>
+        <meta name="google-site-verification" content="ojrMCa-WpK0Vy08VcOeDGmV6KMrBtlUueQNTUqZs8To" />
         <title>Pradeep Shah - Full Stack Developer</title>
         <meta name="description" content="A passionate Full Stack Developer with expertise in building modern web applications. With a strong foundation in both frontend and backend technologies, I create seamless user experiences and robust server-side solutions." />
         <meta name="keywords" content="Pradeep Shah, Full Stack Developer, Web Developer, Software Engineer, Open Source Contributor, Portfolio, React Developer, Node.js Developer" />
         <meta name="author" content="Pradeep Shah" />
-        <meta name="google-site-verification" content="ojrMCa-WpK0Vy08VcOeDGmV6KMrBtlUueQNTUqZs8To" />
+        
         </>
     );  
 }
+
+
